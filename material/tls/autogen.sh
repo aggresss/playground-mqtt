030f128babce179ece8411c9b92b5b5d8bb7bc42
aggresss <aggresss@163.com>
2019-04-28 21:02:15 +0800

    2019-04-28 21:02:14 CST W16D7

--- material/tls/autogen.sh
+++ material/tls/autogen.sh
@@ -1,8 +1,12 @@
+#!/usr/bin/env bash
+# When OpenSSL prompts you for the Common Name for each certificate, use different names.
+
 # CA 相关
+
 # CA key
 openssl genrsa -out ca.key 2048
 # CA csr
-openssl req -new -key ca.key -out ca.csr
+openssl req -new -subj "/CN=ca" -key ca.key -out ca.csr
 # CA crt
 openssl x509 -req -in ca.csr -out ca.crt -signkey ca.key -days 3650
 
@@ -11,14 +15,14 @@ openssl x509 -req -in ca.csr -out ca.crt -signkey ca.key -days 3650
 # server key
 openssl genrsa -out server.key 2048
 # server.csr
-openssl req -new -key server.key -out server.csr
+openssl req -new -subj "/CN=server" -key server.key -out server.csr
 # server.crt
 openssl x509 -req -in server.csr -out server.crt -CA ca.crt -CAkey ca.key -CAcreateserial -days 3650
 
 # client key
 openssl genrsa -out client.key 2048
 # client.csr
-openssl req -new -key client.key -out client.csr
+openssl req -new -subj "/CN=client" -key client.key -out client.csr
 # client.crt
 openssl x509 -req -in client.csr -out client.crt -CA ca.crt -CAkey ca.key -CAcreateserial -days 3650
 
