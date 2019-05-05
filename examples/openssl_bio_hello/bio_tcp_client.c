45ea4810ec0cd54e4fbc51b7ead35a1924bf91e0
aggresss <aggresss@163.com>
2019-05-05 10:19:12 +0800

    2019-05-05 10:19:11 CST W17D7

--- examples/openssl_bio_hello/bio_tcp_client.c
+++ examples/openssl_bio_hello/bio_tcp_client.c
@@ -5,11 +5,21 @@ int main()
     int len;
     char tmpbuf[1024];
 
-    cbio = BIO_new_connect("localhost:http");
+    cbio = BIO_new_connect("localhost:55555");
+    if(cbio == NULL) {
+        fprintf(stderr, "Error create new conncet\n");
+    }
+
     out = BIO_new_fp(stdout, BIO_NOCLOSE);
+    if(out == NULL) {
+        fprintf(stderr, "Error create new fp\n");
+    }
+
     if (BIO_do_connect(cbio) <= 0) {
         fprintf(stderr, "Error connecting to server\n");
+        return -1;
     }
+
     BIO_puts(cbio, "GET / HTTP/1.0\n\n");
     for (;;) {
         len = BIO_read(cbio, tmpbuf, 1024);
--- examples/openssl_bio_hello/bio_tcp_server.c
+++ examples/openssl_bio_hello/bio_tcp_server.c
@@ -1,25 +1,44 @@
 #include <stdio.h>
 #include <string.h>
 #include <openssl/bio.h>
+#include <openssl/err.h>
 
 int main()
 {
-    BIO *b = NULL, *c = NULL;
-    int sock, ret, len;
-    char *addr = NULL;
-    char out[80];
-
-    sock = BIO_get_accept_socket("2323", 0);
-    b = BIO_new_socket(sock, BIO_NOCLOSE);
-    ret = BIO_accept(sock, &addr);
-    BIO_set_fd(b, ret, BIO_NOCLOSE);
-    while (1) {
-        memset(out, 0, 80);
-        len = BIO_read(b, out, 80);
-        if (out[0] == 'q')
-            break;
-        printf("%s", out);
+    BIO *abio, *cbio, *cbio2;
+    ERR_load_BIO_strings();
+    abio = BIO_new_accept("55555");
+    /* First call to BIO_accept() sets up accept BIO */
+    if (BIO_do_accept(abio) <= 0) {
+        fprintf(stderr, "Error setting up accept\n");
+        ERR_print_errors_fp(stderr);
+        exit(0);
     }
-    BIO_free(b);
-    return 0;
+    /* Wait for incoming connection */
+    if (BIO_do_accept(abio) <= 0) {
+        fprintf(stderr, "Error accepting connection\n");
+        ERR_print_errors_fp(stderr);
+        exit(0);
+    }
+    fprintf(stderr, "Connection 1 established\n");
+    /* Retrieve BIO for connection */
+    cbio = BIO_pop(abio);
+    BIO_puts(cbio, "Connection 1: Sending out Data on initial connection\n");
+    fprintf(stderr, "Sent out data on connection 1\n");
+    /* Wait for another connection */
+    if (BIO_do_accept(abio) <= 0) {
+        fprintf(stderr, "Error accepting connection\n");
+        ERR_print_errors_fp(stderr);
+        exit(0);
+    }
+    fprintf(stderr, "Connection 2 established\n");
+    /* Close accept BIO to refuse further connections */
+    cbio2 = BIO_pop(abio);
+    BIO_free(abio);
+    BIO_puts(cbio2, "Connection 2: Sending out Data on second\n");
+    fprintf(stderr, "Sent out data on connection 2\n");
+    BIO_puts(cbio, "Connection 1: Second connection established\n");
+    /* Close the two established connections */
+    BIO_free(cbio);
+    BIO_free(cbio2);
 }
