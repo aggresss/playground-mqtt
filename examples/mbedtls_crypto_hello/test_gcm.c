04e71f2d244513463f24e1486c322e13a428dab0
aggresss <aggresss@163.com>
2019-06-11 16:01:49 +0800

    2019-06-11 16:01:49 CST W23D2

--- examples/mbedtls_crypto_hello/test_gcm.c
+++ examples/mbedtls_crypto_hello/test_gcm.c
@@ -2,34 +2,34 @@
 #include <stdlib.h>
 #include <string.h>
 #include "mbedtls/gcm.h"
-#include "mbedtls/md5.h"
+#include "mbedtls/sha1.h"
 #include "b64/urlsafe_b64.h"
 
-#define MD5_LEN 16
+#define SHA1_LEN 20
 #define NONCE_LEN 12
 #define GCM_TAG_LEN 16
 
 const char input[] =
-        "KEtxNMjaVARzb1GDqdwD-6_xXAnjrMDKLSFFiWLTXccN8LT3waCcNqYUie4iRAlXFYA5UL5tb4TqE-CP-HD7z2qg4i4Xg7dhAjeS_Iu7-nTALTui8bdZZum-BpJsb98F_hkgny2qJSB3zDpyCWAFXe-oB4zFPhHmgpgNGCAe9X-szlD2qefXo_DMO5iOBXzCUbeTKhF9TIeygE0BapY_I2FYoMYwRLyyoBRY7Bf5PfkKoM6WdGWmOrkK1zoz7rF504NQvprXtKqtMy-BtjhZ7RcDEcu2J6INQ00zWtf6QBFNec6gI_LVRsKiZuLIow7rvsdAANJMTx37pKLeamaSacUWA2-5rDOHuNNF_O52KJwkYKnOs-V0ZlJPKvgeKlmXPs66Ab19Lm4uivDYBh98hrgLn8Gu1X5K57P8MM8VP773yinIYRBhgcjmDK6MKhQ32p5i2uq3hSX__CdLTZhfisi4GZRZr1E309oWFWOvrNKGnncFN4_TSGEi0dtFsI0QfElHuWRhsigznI_FAuENgOsqi_DIKhwLwCkrhNtViK5I-U9ia2w_ypG7d455P_zGONRCtQjLqvEAYcVFRFIUXX29n6K6zW4Uip-CHssmSufWXo-b6fVKoWcASr_5pgvsLWXMva3fu8xCvwmsXg7psBBYQ9uemGOlCucNEpTkATO8DSF06oP7IDuqzN6zNWLOPLCxzvT0x7k3RhqClfC73jQxNp8Ha1Lp1o_SV330Gyyjsop8DVlXQasfPgKTayhhWZ2lj2NmDQinj9xrYNB5jVNW1prERD_lHdyS90lg19k0RJ5nHXaJc_kNA7B8M9U1Krvxekcwx7pq19tS5h_pB11arADfROEHBbHgMeURfZY1MMcV1oI-qr-mJS-auTRB3yBwqOXi-NA09SPhLciQqq8TRClKhVbu558EMdm7AWcSUhS01L983FRQvG9hK6dBkEyh4hPTTPwL0wTh0wM73BRxSv_GWXwhXNsFNsOSCIK9KE1uN-C9_5OlM7zVDk5idaKiNkutnyYLJIiswqJrT-8IbEA_cv8NppEdx1MgaNHn2W_eAc-zVCcEB59b52sbw7xo2aOiatZqz0ykJA6nWHSF1Xcoy4lgLWgBdAyApXF89Y-MGs6M6DmRE56Pu-oCI7dBMQ7Z-Rk9Oo_LpWokx52MIDaaeMOV5PVYjQEw8xkge7-OcDK_rvEs_gGAnLNPf4sCMzoWes7cuZ_G28ZkxuordU7iqiKHpCp0bgltM6prNvXBmIFAcgI3G0BN7n0aV01hEERkjTD50CdBOneA5Ol-peyGDSY=";
-const char dsk[] = "uWxCtA04ZKI0IFn10DGlcdNixAHELUqqiQ60kzfW";
+            "s76k9mKx_SKW-9JR_A7SoUp0AYobAcbVWBCZXNBpsgYgpc9OD_7f8WRyohz1iwyN9EFUxRMips_9ntVLhBfFm9iLRWasR61A8EiyJQr8GLrwqiYAMsDqn05Tl_ELZJLzetii76PNmMEur7hHB9TtVGS9-unSC2RFmuhpJ2pS5Lo9wD8TYrglnSpxurad28Wu_OV42WVWb3dsL8dkHd6ziIQl6AfA3YWT5ycZ_rw2YypdRhNaAyd3LENX2hpJ7UFCfkAE-LmQsj_2JQyxP3tvXbIMjnNiqoV32rB-GKEGfdftQSs6N3kZ2LTezx1pC4-lNZPmzCECWSAwxUiL3PYRCKKR_o3qa88tYzybDvnpDGbW-cgQ8fkfvCX1hW2Kg_OWTO9-Y9ud5-dkJF1_4qU9orxzfuD5CxUAafbqAtDuKkjvDY9xQHQl2euHdl7JwLVvEgAlq6mIYPjPEqfptFgOYl95hhWHz7Ok5IgHKAhc91a8PX-ycM0RiF49A7fd5biy_7PFki3Q8OL4jbBpAqF3X922L-tBqJ5ZmKPadphzwSRp7rBaSti5RE83JTZAsDFJXIOlY5_FWNS2jEP5g1fAmIfzRPDk5Rlid10FLins2myl5k4CDUuJDGClqC7EjO_bslxOQbIvEOYfU7L1KDpO8Z928ohQuAc4ZHwTiCf8duFyiDJb0dEtusF5tel-5e2qTlRccWdzXPit88kIrWlPbWigNhJQV0aN8E4ayit_yuKSTvZwxTt78coowvp2aR-IMeMOLGURjchTflEBadsnYKcFeFGXMVpuMxV-SrBurrE1T59Cr15daaT1_wYbrDt-JA7nsmCOTLHLVMafIzWOkft7v55FNfMoDZKR-5IWCNQKd7JTf-Y2B7jjjpvcnsKUyvhE7-LGDb314GcHjN3cfOYP2fZTzq0dqweVruSYRUbq5mkPtACSzr9e1kThUdd3qGC5Ew5Eeidvi4de8-WG8u_flh7h24c4_rt4aJsXDnVRUD1ZJ-tkjHxjMipNqEgT3tI_-_CxqaJvUoTR98ZY_P966ySX6tf9_MJVDcJsChs-c8oDGo-jEWZAvvuX1hCnqjJG5YDQCzmVGdG7CWQilaRu0_1ftaPf6etbN-X5jnfBu7iHKGwTeGUfIp71RgSjWoH8G2eC2n1umPXVa-HUnFoya2kkyVJ9gFkoPktxizK3-t1_kdz4KpSPMp-YDdL13vQZcFJOFpcvHeGkAaaaLqrWppxeU-0QmbGcNEoPtY5umZf1yzFsJm63_cvQosLRyQST2eykXxKE93-7r5_v9XKsi9JtiNs=";
+        const char dsk[] = "uWxCtA04ZKI0IFn10DGlcdNixAHELUqqiQ60kzfW";
 
 int main()
 {
     size_t intput_len = strlen(input);
     unsigned char un_b64[4096] = { 0 };
     size_t un_b64_len = 4096;
-    unsigned char dsk_md5[MD5_LEN] = { 0 };
+    unsigned char dsk_sha1[SHA1_LEN] = { 0 };
     unsigned char nonce[NONCE_LEN] = { 0 };
-    mbedtls_md5_context md5_ctx;
+    mbedtls_sha1_context sha1_ctx;
     mbedtls_gcm_context gcm_ctx;
     unsigned char gcm_tag[GCM_TAG_LEN] = { 0 };
     unsigned char output[4096] = { 0 };
 
     /* Step 1: Get md5sum of dsk */
-    mbedtls_md5_init(&md5_ctx);
-    mbedtls_md5_starts(&md5_ctx);
-    mbedtls_md5_update_ret(&md5_ctx, (const unsigned char*) dsk, strlen(dsk));
-    mbedtls_md5_finish(&md5_ctx, dsk_md5);
+    mbedtls_sha1_init(&sha1_ctx);
+    mbedtls_sha1_starts(&sha1_ctx);
+    mbedtls_sha1_update_ret(&sha1_ctx, (const unsigned char*) dsk, strlen(dsk));
+    mbedtls_sha1_finish(&sha1_ctx, dsk_sha1);
 
     /* Step 2: b64 decode */
     un_b64_len = urlsafe_b64_decode(input, intput_len, un_b64, un_b64_len);
@@ -47,8 +47,8 @@ int main()
     /* Step 4: AES-GCM decrypto */
     mbedtls_gcm_init(&gcm_ctx);
     mbedtls_gcm_setkey(&gcm_ctx, MBEDTLS_CIPHER_ID_AES,
-            (const unsigned char*) dsk_md5,
-            MD5_LEN * 8);
+            (const unsigned char*) dsk_sha1,
+            128);
     mbedtls_gcm_starts(&gcm_ctx, MBEDTLS_GCM_DECRYPT,
             (const unsigned char*) nonce,
             NONCE_LEN, NULL, 0);
