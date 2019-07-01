d20d2fba3d21f6d851faf2cbeb4b5e39112fa22c
aggresss <aggresss@163.com>
2019-07-01 14:37:49 +0800

    2019-07-01 14:37:49 CST W26D1

---
+++ examples/paho_heap_test/paho_heap_test.c
@@ -0,0 +1,40 @@
+#include "Heap.h"
+#include "Log.h"
+
+static void traceCallback(enum LOG_LEVELS level, const char* message)
+{
+    printf("[ %d ]: %s\n", level, message);
+    return;
+}
+
+int main()
+{
+
+    (void)Heap_initialize();
+    (void)Log_initialize(NULL);
+
+    heap_info *hi = Heap_get_info();
+
+    char *buf1 = malloc(1024);
+
+    char *buf2 = malloc(1024);
+
+    char *buf3 = malloc(1024);
+
+    Log_setTraceLevel(TRACE_MAXIMUM);
+    Log_setTraceCallback(traceCallback);
+
+    Log(TRACE_PROTOCOL, -1, "Current heap size: %zu, max heap size: %zu.\n", hi->current_size, hi->max_size);
+
+    free(buf1);
+
+    free(buf2);
+
+    free(buf3);
+
+    Log(TRACE_PROTOCOL, -1, "Current heap size: %zu, max heap size: %zu.\n", hi->current_size, hi->max_size);
+
+    Heap_terminate();
+
+    return 0;
+}
